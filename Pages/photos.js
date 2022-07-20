b45ac26ab2b528b4e8a02383427cb7c92060c900
Jess Rinehart <jrrinehart@outlook.com>
2022-07-20 19:52:50 -0400

    added a JS page and moved photos to own folder

---
+++ Pages/photos.js
@@ -0,0 +1,49 @@
+}const grid = document.getElementById("grid");
+
+//console.log(grid);
+
+grid.innerHTML =  `<img src="../Photos\Sunflower.jpg" alt="Sunflower">`;
+
+const imgArray = [
+    {
+        src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   
+        src: 'Photos\Ducks.jpg'
+        alt: "A mother duck and her ducklings swimming in the Licking River",
+        tags: ["Covington", "photo", "duck"]
+    }
+    {
+        src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   
+        src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }
+    {   
+        src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   
+        src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }
+];
+
+
+
+imgArray.map((img) => {
+   imgHTML += `<img src=${img.src} alt=${img.alt}>`;
+   //temperate literal
+});
+
+grid.innerHTML = imgHTML;
+
+//filter array with textbox for tags element
