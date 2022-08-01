45e771a9ebc5d35e62ea3efc41db58769771fc71
riniejr <105397836+riniejr@users.noreply.github.com>
2022-08-01 03:19:37 -0400

    Add files via upload

---
+++ Pages/Pages/photos.js
@@ -0,0 +1,53 @@
+ const grid = document.getElementById("grid");
+
+//console.log(grid);
+
+// Work on arrays & tags
+
+//grid.innerHTML =  `<img src="../Photos\Sunflower.jpg" alt="Sunflower">`;
+
+//const imgArray = [
+    {   src: '../Photos\Sunflower.jpg'
+        _alt: "Sunflower",
+        get alt() {
+            return this._alt;
+        },
+        set alt(value) {
+            this._alt = value;
+        },
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   src: 'Photos\Ducks.jpg'
+        alt: "A mother duck and her ducklings swimming in the Licking River",
+        tags: ["Covington", "photo", "duck"]
+    },
+    {   src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    },
+    {   src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    }, 
+    {   src: '../Photos\Sunflower.jpg'
+        alt: "Sunflower",
+        tags: ["sunflower", "photo", "flower"]
+    },
+];
+
+
+
+imgArray.map((img) => {
+   imgHTML += `<img src=${img.src} alt=${img.alt}>`;
+   //temperate literal
+});
+
+grid.innerHTML = imgHTML;
+
+//filter array with textbox for tags element
+
+
